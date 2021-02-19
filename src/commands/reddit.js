fd956c268724882a5547cf184f9a41db2bbad160
Elias Jackson <ejthedj0@gmail.com>
2021-02-19 15:03:18 -0800

    Reddit error handling

--- src/commands/reddit.js
+++ src/commands/reddit.js
@@ -18,25 +18,32 @@ class RedditCommand extends Command {
   }
 
   async exec(message, { input }) {
-    const [subreddit, ...queryWords] = input.split(' ');
+    const [subredditName, ...queryWords] = input.split(' ');
     const query = queryWords.join(' ');
 
     const r = new snoowrap(config.reddit);
 
-    let searchResults;
-    if(query.length > 0) {
-      searchResults = await r.search({
-        query,
-        subreddit,
-        restrictSr: true,
-        limit: 5
-      });
-    }
-    else {
-      searchResults = await r.getSubreddit(subreddit).getHot({limit: 3});
+    const subreddit = await r.getSubreddit(subredditName).fetch().catch(() => {
+      return undefined;
+    });
+    if(!subreddit) {
+      return message.channel.send(`Subreddit /r/${subredditName} not found.`);
     }
 
-    // TODO: If subreddit doesn't exist, return error message
+    let searchResults;
+    if(query.length > 0) {
+      searchResults = await subreddit.search({
+        query,
+        limit: 4
+      });
+      if(searchResults.length === 0) {
+        return message.channel.send(`No results found for ${query} in /r/${subredditName}`);
+      }
+    }
+    else {
+      searchResults = await subreddit.getHot({limit: 3});
+    }
+
 
     return message.channel.send({
       embed: {
@@ -45,11 +52,11 @@ class RedditCommand extends Command {
           icon_url: 'https://styles.redditmedia.com/t5_2r0ij/styles/communityIcon_yor9myhxz5x11.png',
           name: `Reddit`,
         },
-        url: `https://reddit.com/r/${subreddit}`,
+        url: `https://reddit.com/r/${subredditName}`,
         fields: [
           {
-            name: query.length > 0 ? `/r/${subreddit} search: "${query}"` : `Hot posts in /r/${subreddit}`,
-            value: searchResults.map(formatPost).join('\n')
+            name: query.length > 0 ? `/r/${subredditName} search: "${query}"` : `Hot posts in /r/${subredditName}`,
+            value: searchResults.map(formatPost).join('\n').substr(0, 1023)
           }
         ],
         timestamp: new Date(),
